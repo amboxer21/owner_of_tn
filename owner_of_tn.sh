c91b4a451de21a9389de0c80080fc511a176e366
Anthony Guevara <amboxer21@users.noreply.github.com>
2016-05-24 13:51:25 -0400

    Delete owner_of_tn.sh

--- owner_of_tn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sshpass -p 'FISH scale 21' ssh nobody@200.255.100.118 "ruby /home/anthony/Documents/Ruby/owner_of_tn.rb $1"
-ssh -t aguevara@hosted "bash /home/aguevara/Documents/Bash/owner_of_tn.sh $1"
