5dadf5fa8976de33186866d1bf3517e2790e2e5c
Anthony Guevara <amboxer21@gmail.com>
2016-05-25 10:45:34 -0400

    Added argument check.

--- owner_of_tn.sh
+++ owner_of_tn.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [[ $# != 1 ]]; then
+  echo -e "\n => Usage: ./owner_of_tn.rb <telephone number>\n";
+  exit;
+fi
+
 sshpass -p 'FXXXxxxxxxxxx' ssh nobody@200.255.xxx.xxx "ruby /home/anthony/Documents/Ruby/owner_of_tn.rb $1"
 ssh -t aguevara@hosted "bash /home/aguevara/Documents/Bash/owner_of_tn.sh $1"
