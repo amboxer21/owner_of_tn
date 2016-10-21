5c9d1e42ffb4f016b4bead6bc4f4be750ec9ef7c
Anthony Guevara <amboxer21@users.noreply.github.com>
2016-10-21 10:53:09 -0400

    Update owner_of_tn_fs.rb

--- owner_of_tn_fs.rb
+++ owner_of_tn_fs.rb
@@ -15,23 +15,24 @@ def display
   puts "\n - Checking the feature servers now.\n\n"
 end
 
-def login_and_query(command,forward_check)
+def login_and_query(number)
 
   @username = 'agxxxxxx'
-  @password = 'LXXXXXXXXXX'
+  @password = 'LIMAxxxxxxx'
 
-  $feature_servers, $results, $node = [], [], []
-  $feature_servers.push('fsa.xxx.xxx','cm-fsa.xxx.xxx','pl-fsa.xxx.xxx')
+  $feature_servers, $results, $ext_file, $nodeName, $node = [], [], [], [], []
+  $feature_servers.push('fsa.xxxxxxxx.com','cx-fsa.xxxxxxxx.com','px-fsa.xxxxxxxx.com')
 
   $feature_servers.each do |i|
     @hostname = i
     begin
       Net::SSH.start(@hostname, @username, :password => @password ) do |ssh|
-        command_res = ssh.exec!(command)
-        $node.push    ssh.exec!("uname -n") if !command_res.nil?
-        command_res = command_res.scan(Regexp.union(/[0-9]+.ext:/,/[0-9]+.num:/,/[0-9]+.sip:/)).uniq.to_s unless command_res.nil?
-        command_res = command_res.scan(Regexp.union(/[0-9]+.ext/,/[0-9]+.num/,/[0-9]+.sip/))              unless command_res.nil?
-        $results.push command_res unless command_res.nil?
+        $node.push ssh.exec!("uname -n")
+        numberGrep = ssh.exec!("egrep #{number} /etc/asterisk/customer/*")
+        $nodeName.push ssh.exec!("uname -n") if !numberGrep.nil?
+        file = numberGrep.scan(/[0-9]+.num:/).uniq.to_s.scan(/[0-9]+\.num/) unless numberGrep.nil?
+        $results.push numberGrep unless numberGrep.nil?
+        puts " => #{$arg} not found on the #{$node.last.gsub(/\n+/,'')} feature server." if $nodeName.empty?
       end
     rescue => e
       puts "Error: #{e}"
@@ -39,22 +40,25 @@ def login_and_query(command,forward_check)
   end
 
   $results.each do |line|
-    $account_number = line.first.to_s.gsub(/.(num|ext|sip)/,"")
+    if line =~ /#{$arg}/
+      $ext_file.push line.scan(/[0-9]+.num:/).uniq.to_s.scan(/[0-9]+\.num/)
+      $account_number = line.scan(/[0-9]+.num:/).uniq.to_s.scan(/[0-9]+/)
+    end
   end
 
-  #$nodeName.each do |node|
-  $node.each do |n|
-    if !$account_number.nil?
-    puts "\n -> Found number #{$arg}"
-      puts " -> For account:  #{$account_number.to_s.gsub(/[\[\"\]]/,'')}"
-      puts " -> On the #{n.gsub(/\n+/,'')} feature server."
+  $nodeName.each do |node|
+
+    if $account_number.to_s.gsub(/[\[\"\]]/,'').empty?
+      puts "\n -> Number #{$arg} not found"
+      puts " -> On the #{node.gsub(/\n+/,'')} feature server."
     else
-      puts " => #{$arg} not found on the #{n.last.gsub(/\n+/,'')} feature server."
+      puts "\n -> Found number #{$arg}"
+      puts " -> For account:  #{$account_number.to_s.gsub(/[\[\"\]]/,'')}"
+      puts " -> On the #{node.gsub(/\n+/,'')} feature server."
     end
   end
 
 end
 
 display
-login_and_query("egrep #{$arg} /etc/asterisk/customer/*",false)
-#login_and_query("asterisk -rx 'database show CFV' | egrep #{$arg}",true)
+login_and_query($arg)
