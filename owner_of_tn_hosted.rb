b0c7ce506b1dbd96a26886be66f70e9ab9312780
Anthony Guevara <amboxer21@users.noreply.github.com>
2016-10-28 12:35:30 -0400

    Update owner_of_tn_hosted.rb

--- owner_of_tn_hosted.rb
+++ owner_of_tn_hosted.rb
@@ -2,6 +2,7 @@
 require "/var/asterisk/hosted/current/hpbxgui/config/environment.rb"
 
 @flag         = "true"
+@fwd_flag     = "true"
 @e911_flag    = "true"
 @ucid_flag    = "true"
 @umobile_flag = "true"
@@ -19,39 +20,51 @@ else
 end
  
 User.all.each do |ext|
-  unless ext.e911_callerid.nil?
+  next if ext.e911_callerid.nil?
     if ext.e911_callerid =~ /#{@tn}/
       puts "\n -> Found e911 number #{ext.e911_callerid}"
       puts " -> For user #{ext.name}"
       puts " -> For account ##{Account.find_by(tenant_id: ext.tenant_id).account_number}"
       puts " -> For workgroup #{Location.find_by(tenant_id: ext.tenant_id).name}.\n\n" 
-      @e911_flag = "true"                                                                                                                                                        
-    else                                                                                                                                                                         
-      @e911_flag = "false"                                                                                                                                                       
-    end                                                                                                                                                                          
-  end                                                                                                                                                                            
-  unless ext.callerid.nil?                                                                                                                                                       
-    if ext.callerid =~ /#{@tn}/                                                                                                                                                  
-      puts "\n -> Found CID number #{ext.callerid}"                                                                                                                              
-      puts " -> For user #{ext.name}"                                                                                                                                            
-      puts " -> For account ##{Account.find_by(tenant_id: ext.tenant_id).account_number}"                                                                                        
-      puts " -> For workgroup #{Location.find_by(tenant_id: ext.tenant_id).name}.\n\n"                                                                                           
+      @e911_flag = "true"
+    else
+      @e911_flag = "false"
+    end
+
+  next if ext.callerid.nil?
+    if ext.callerid =~ /#{@tn}/
+      puts "\n -> Found CID number #{ext.callerid}"
+      puts " -> For user #{ext.name}"
+      puts " -> For account ##{Account.find_by(tenant_id: ext.tenant_id).account_number}"
+      puts " -> For workgroup #{Location.find_by(tenant_id: ext.tenant_id).name}.\n\n" 
       @ucid_flag = "true"
     else
       @ucid_flag = "false"
     end
-  end
-  unless ext.mobile_tn.nil?
+
+  next if ext.mobile_tn.nil?
     if ext.mobile_tn =~ /#{@tn}/
-      puts "\n => FOund mobile number #{ext.mobile_tn}\n"
+      puts "\n -> Found mobile number #{ext.mobile_tn}\n"
       puts " -> For user #{ext.name}"
-      puts " -> For account ##{Account.find_by(tenant_id: ext.tenant_id).account_number}".
+      puts " -> For account ##{Account.find_by(tenant_id: ext.tenant_id).account_number}."
       puts " -> For workgroup #{Location.find_by(tenant_id: ext.tenant_id).name}.\n\n" 
       @umobile_flag = "true"
     else
       @umobile_flag = "false"
     end
+
+  next if ext.ami_hash.nil?
+  ext.ami_hash.each do |key,val|
+    if key =~ /CFAN/ && val =~ /#{@tn}/
+      puts "\n -> Found forward number #{val}\n -> For user: #{ext.name}" unless ext.nil?
+      puts " -> For account ##{Account.find_by(tenant_id: ext.tenant_id).account_number}."
+      puts " -> For workgroup #{Location.find_by(tenant_id: ext.tenant_id).name}.\n\n"
+      @fwd_flag = "true"
+    else
+      @fwd_flag = "false"
+    end
   end
+
 end
 
 puts "\n => User E911 number not found.\n\n" unless @e911_flag == "true"
@@ -62,6 +75,9 @@ puts "\n => User CID number not found.\n\n" unless @ucid_flag == "true"
 
 puts "\n => User mobile number not found.\n\n" unless @umobile_flag == "true"
 @umobile_flag = false
+
+puts "\n => User Forward number not found.\n\n" unless @fwd_flag == "true"
+@fwd_flag = false
 
 Tenant.all.each do |user|
   if user.callerid =~ /#{@tn}/
